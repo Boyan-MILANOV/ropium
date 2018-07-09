0b59f934c81957d980737cef5d75223880b9972a
Boyan-MILANOV <boyan.milanov@hotmail.fr>
2018-07-09 15:15:36 +0100

    Added IO module, Main module, created new ascii art

---
+++ ropgenerator/IO.py
@@ -0,0 +1,139 @@
+# -*- coding:utf-8 -*- 
+
+from random import random  
+import sys
+
+
+DEFAULT_ROPGENERATOR_COLOR_ANSI = '\033[92m'    # Default color 
+DEFAULT_ERROR_COLOR_ANSI = '\033[91m' 
+DEFAULT_BOLD_COLOR_ANSI = '\033[1m'
+DEFAULT_SPECIAL_COLOR_ANSI = '\033[93m'
+DEFAULT_PAYLOAD_COLOR_ANSI = '\033[96m'
+DEFAULT_EXPLOIT_COLOR_ANSI = '\033[95m'
+DEFAULT_END_COLOR_ANSI = '\033[0m'
+
+ROPGENERATOR_COLOR_ANSI = DEFAULT_ROPGENERATOR_COLOR_ANSI
+ERROR_COLOR_ANSI = DEFAULT_ERROR_COLOR_ANSI
+BOLD_COLOR_ANSI = DEFAULT_BOLD_COLOR_ANSI
+SPECIAL_COLOR_ANSI = DEFAULT_SPECIAL_COLOR_ANSI
+PAYLOAD_COLOR_ANSI = DEFAULT_PAYLOAD_COLOR_ANSI
+EXPLOIT_COLOR_ANSI = DEFAULT_EXPLOIT_COLOR_ANSI
+END_COLOR_ANSI = DEFAULT_END_COLOR_ANSI
+    
+    
+# Info and errors 
+    
+def notify(text):
+    """
+    prints a string with a tab and special colored char in front 
+    """  
+    sys.stdout.write('\t'+ROPGENERATOR_COLOR_ANSI + '% ' + END_COLOR_ANSI + text+'\n')    
+
+def info(text):
+    """
+    Prints a text with a colored '[+] ' before 
+    """
+    sys.stdout.write(ROPGENERATOR_COLOR_ANSI + '[+] ' + END_COLOR_ANSI + text)
+    
+def error(text):
+    """
+    Prints a text with the error color
+    """
+    sys.stdout.write(ERROR_COLOR_ANSI + '[!] ' + text + END_COLOR_ANSI)
+    
+VERBOSE = False   # For verbose info support 
+def verbose_mode(mode):
+    """
+    mode = True or False
+    """
+    global VERBOSE
+    VERBOSE = mode
+ 
+def info_verbose(msg):
+    if( VERBOSE ):
+        print('\t\t> '+msg)
+        
+        
+def banner(msg):
+    chars = [u'\u2591', u'\u2592', u'\u2593']
+    l1 = ''.join([chars[int(((random()-0.01)*10 - 1)/3)] for _ in range(0, len(msg))])
+    l2 = ''.join([chars[int(((random()-0.01)*10 - 1)/3)] for _ in range(0, len(msg))])
+    return '\n' + ROPGENERATOR_COLOR_ANSI + '\t'+ l1 +'\n' + END_COLOR_ANSI\
+            + '\t' + string_bold(msg) + '\n'\
+            + ROPGENERATOR_COLOR_ANSI + '\t'+ l2 +'\n'
+    
+# Coloring strings 
+
+def string_bold(text):
+    """
+    Returns a string in bold
+    """
+    return BOLD_COLOR_ANSI+text+END_COLOR_ANSI
+
+def string_special(text):
+    """
+    Returns a string with special color 
+    """
+    return SPECIAL_COLOR_ANSI+text+END_COLOR_ANSI
+    
+def string_payload(text):
+    """
+    Returns a string with special color for payload
+    """
+    return ROPGENERATOR_COLOR_ANSI+text+END_COLOR_ANSI
+
+def string_ropg(text):
+    return ROPGENERATOR_COLOR_ANSI+text+END_COLOR_ANSI
+
+def string_exploit(text):
+    return EXPLOIT_COLOR_ANSI + text + END_COLOR_ANSI
+    
+# Custom charging bar :) 
+last_percent = -1 
+def charging_bar( nb_iter, curr_iter, bar_len, msg="", char=u"\u2588"):
+    """
+    Print a charging bar 
+    """
+    global last_percent
+    percent = (100*curr_iter)/nb_iter
+    if( curr_iter == nb_iter ):
+        sys.stdout.write('\r'+' '*(bar_len+len(msg)+30)+'\r')
+        last_percent = -1
+    elif( last_percent != percent):
+        last_percent = percent
+        bar = '\r\t'+ROPGENERATOR_COLOR_ANSI + '% ' + END_COLOR_ANSI
+        bar += str(msg)
+        bar += ' |'
+        full_part = char * (curr_iter/(nb_iter/bar_len))
+        empty_part = " "*(bar_len-len(full_part))
+        bar += full_part + empty_part + '| '
+        bar += '{:03d}%'.format(100*curr_iter/nb_iter)
+        sys.stdout.write(bar)
+    sys.stdout.flush()
+
+
+# Enable / Disable ANSI output colors 
+
+def disable_colors():
+    global ROPGENERATOR_COLOR_ANSI, ERROR_COLOR_ANSI,BOLD_COLOR_ANSI,\
+        SPECIAL_COLOR_ANSI,PAYLOAD_COLOR_ANSI,EXPLOIT_COLOR_ANSI,END_COLOR_ANSI
+    
+    ROPGENERATOR_COLOR_ANSI = ''
+    ERROR_COLOR_ANSI = ''
+    BOLD_COLOR_ANSI = ''
+    SPECIAL_COLOR_ANSI = ''
+    PAYLOAD_COLOR_ANSI = ''
+    EXPLOIT_COLOR_ANSI = ''
+    END_COLOR_ANSI = '' 
+        
+def enable_colors():
+    global ROPGENERATOR_COLOR_ANSI, ERROR_COLOR_ANSI,BOLD_COLOR_ANSI,\
+        SPECIAL_COLOR_ANSI,PAYLOAD_COLOR_ANSI,EXPLOIT_COLOR_ANSI,END_COLOR_ANSI
+    
+    ROPGENERATOR_COLOR_ANSI = DEFAULT_ROPGENERATOR_COLOR_ANSI
+    ERROR_COLOR_ANSI = DEFAULT_ERROR_COLOR_ANSI
+    BOLD_COLOR_ANSI = DEFAULT_BOLD_COLOR_ANSI
+    SPECIAL_COLOR_ANSI = DEFAULT_SPECIAL_COLOR_ANSI
+    PAYLOAD_COLOR_ANSI = DEFAULT_PAYLOAD_COLOR_ANSI
+    EXPLOIT_COLOR_ANSI = DEFAULT_EXPLOIT_COLOR_ANSI
+    END_COLOR_ANSI = DEFAULT_END_COLOR_ANSI
--- ropgenerator/Main.py
+++ ropgenerator/Main.py
@@ -1,3 +1,82 @@
+# -*- coding: utf-8 -*- 
+# Main module: run ROPGenerator 
+
+
+from prompt_toolkit import prompt
+from prompt_toolkit.history import InMemoryHistory
+from prompt_toolkit.contrib.completers import WordCompleter
+
+from ropgenerator.IO import string_ropg, string_bold, banner 
+
+import sys
+
+ASCII_art = """
+▒▒▒▒▒▒▒╗░▒▒▒▒▒▒╗░▒▒▒▒▒▒░░░▒░▒▒ ░ ▒░▒ ░▒░▒▓▒░▒▒▓░▒▓░▓▒░           
+▒▒╔══▒▒║▒▒╔═══▒▒╗▒▒╔══▒╗
+▒▒▒▒▒▒╔╝▒▒║   ▒▒║▒▒▒▒▒▒║ G  E  N  E  R  A  T  O  R 
+▒▒╔══▒▒╗╚▒▒▒▒▒▒╔╝▒▒╔═══╝ 
+╚═╝  ╚═╝ ╚═════╝ ╚═╝                    
+▓▒░ ░░ ░ ░▒▒ ░░▒░▒░  ░▒▓░ ░░░ ▒ ▓░▒ ░ ▒░▒░▒░ ░ ▒▓▒░▒▓░
+▒ ░     ░   ░░ ░░ ░░▒ ░ ▒░▒░ ░   ░▒▒    ░ ▒░   ░▒ ░ ▒░
+   ░ ░░ ░ ░   ░      ░    ░   ░ ░ ░ ░    ░░   ░ ░ ░
+         ░    ░   ░ ░   ░    ░  ░ ░        ░ ░     ░
+
+"""
+
+
+# Definitions of commands 
+CMD_HELP = "help"
+CMD_LOAD = "load"
+CMD_CONFIG = "config"
+CMD_EXIT = "exit"
+
+CMD_SEARCH = "search-mode" 
+CMD_EXPLOIT = "exploit-mode"
+
+
+command_list = [CMD_HELP, CMD_LOAD, CMD_CONFIG, CMD_EXIT, CMD_SEARCH, CMD_EXPLOIT]
+command_completer = WordCompleter(command_list)
+command_history = InMemoryHistory()
+
+helpStr = banner('ROPGenerator main commands')
+
 def main():
-    print("Blablabli")
+    print(string_ropg(string_bold(ASCII_art)))
+    
+    finish = False
+    while( not finish ):
+        try:
+            sys.stdout.write("("+ string_ropg('main') +")> ")
+            user_input = prompt(u"", history=command_history)
+            args = user_input.split()
+            argslen = len(args)
+            if( argslen > 0 ):
+                command = args[0]
+            else:
+                command = None
+
+            if( command == CMD_EXIT ):
+                finish = True
+            elif( command == CMD_HELP ):
+                print(helpStr)
+            
+            if( command != None ):
+                print('')
+                
+            
+                
+        except KeyboardInterrupt:
+            pass
     exit(0)
+
+
+
+
+
+
+
+
+
+
+
+
