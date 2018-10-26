83dfd0a4a993e0fc35e6e0ce72b2a5b7e1ba3331
Boyan-MILANOV <boyan.milanov@hotmail.fr>
2018-10-26 16:39:59 +0200

    Changed test_record

--- test/test_record.py
+++ test/test_record.py
@@ -3,4 +3,6 @@ from ropgenerator.semantic.Engine import RecordREGtoREG
 r = RecordREGtoREG()
 r.add(0,1,0, [0,1,16,15])
 print(r.regs)
-print( r.check(0,1,0,[0,1])  ) 
+r.add(0,1,0, [1,16])
+print(r.regs)
+print( r.check(0,1,0,[1,16,17])  ) 
