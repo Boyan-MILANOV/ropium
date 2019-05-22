5a6e4bfcda5c8acb51af762d591bf8e78eaf12f5
Boyan-MILANOV <boyan.milanov@hotmail.fr>
2019-05-22 00:00:00 +0000

    Added the ROPGenerator.py script in case ROPGenerator isn't created on case-insensitive fs on macOS

---
+++ ROPGenerator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#import cProfile, pstats
+import ropgenerator.main.Main as Main
+
+#pr = cProfile.Profile()
+#pr.enable()
+Main.main()
+#pr.disable()
+#ps = pstats.Stats(pr).sort_stats('cumulative')
+#ps.print_stats(20)
+
