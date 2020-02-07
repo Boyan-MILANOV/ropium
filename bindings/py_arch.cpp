ff623a5677f718963966d5375baa2222573f610a
Boyan-MILANOV <boyan.milanov@hotmail.fr>
2020-02-07 00:00:00 +0000

    Add ABI enum to bindings

--- bindings/py_arch.cpp
+++ bindings/py_arch.cpp
@@ -1,5 +1,6 @@
 #include "python_bindings.hpp"
 #include "arch.hpp"
+#include "compiler.hpp"
 
 void init_arch(PyObject* module){
     /* ARCH enum */
@@ -10,4 +11,14 @@ void init_arch(PyObject* module){
     PyDict_SetItemString(arch_enum, "ARM64", PyLong_FromLong((int)ArchType::ARM64));
     PyObject* arch_class = create_class(PyUnicode_FromString("ARCH"), PyTuple_New(0), arch_enum);
     PyModule_AddObject(module, "ARCH", arch_class);
+    
+    /* ABI enum */
+    PyObject* abi_enum = PyDict_New();
+    PyDict_SetItemString(abi_enum, "X86_CDECL", PyLong_FromLong((int)ABI::X86_CDECL));
+    PyDict_SetItemString(abi_enum, "X86_STDCALL", PyLong_FromLong((int)ABI::X86_STDCALL));
+    PyDict_SetItemString(abi_enum, "X64_SYSTEM_V", PyLong_FromLong((int)ABI::X64_SYSTEM_V));
+    PyDict_SetItemString(abi_enum, "X64_MS", PyLong_FromLong((int)ABI::X64_MS));
+    PyDict_SetItemString(abi_enum, "NONE", PyLong_FromLong((int)ABI::NONE));
+    PyObject* abi_class = create_class(PyUnicode_FromString("ABI"), PyTuple_New(0), abi_enum);
+    PyModule_AddObject(module, "ABI", abi_class);
 };
